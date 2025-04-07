e38edaeb7f81291a158a0dc1d3641763bfea73bc
Havard buvang <58250866+Havard03@users.noreply.github.com>
2025-04-07 12:48:34 +0200

    fixes (#1)

---
+++ DataBaseConnection.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class DataBaseConnection():
+
+    def __init__(self):
+        self.mysqlConnector = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            name=os.getenv("DB_NAME"),
+        )
+
+    def __enter__(self):
+        try:
+            self.cursor = self.mysqlConnector.cursor()
+            return self
+        except mysql.connector.Error as error:
+            print("Error while connecting to MySQL", error)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.mysqlConnector.commit()
+        self.cursor.close()
+        self.mysqlConnector.close()
--- models/user.py
+++ models/user.py
@@ -2,9 +2,11 @@ from flask_login import UserMixin
 
 
 class User(UserMixin):
+    table = "user"
+    
     def __init__(self, id, firstName, lastName, email):
         self.id = id
         self.firstName = firstName
         self.lastName = lastName
-        self.name = f"{firstName} {lastName}"
         self.email = email
+        self.password = password
