fa46039394784c19267c915f3d284c5bdfe1ada0
Pybruh <ruben.an.lu@gmail.com>
2025-04-10 10:42:32 +0200

    image upload

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_required, current_user
 from database import DataBase
 from models.Event import Event
 from routes.user_management import user_bp
 from models.User import User
 import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/path/to/the/uploads"
+ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg", "gif"])
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 app.register_blueprint(user_bp)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -43,6 +48,10 @@ def my_events():
     )
 
 
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/create-event", methods=["GET", "POST"])
 @login_required
 def create_event():
@@ -60,6 +69,22 @@ def create_event():
             return render_template(
                 "event/create-event.html", error="Something went wrong"
             )
+
+
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
 
         return redirect(url_for("my_events"))
 
