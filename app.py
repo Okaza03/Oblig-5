82fad10f350e444c83e9d803d1f46b41b1123626
Noah Johansen <n@jhns.no>
2025-04-14 13:23:43 +0200

    app fixes

--- app.py
+++ app.py
@@ -1,31 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, login_required, current_user
+from config import app, login_manager, render_template, request
 from database import DataBase
 from models.Event import Event
 from models.User import User
-from routes import event_bp, user_bp
-import os
 
-app = Flask(__name__)
-
-app.secret_key = os.getenv("SECRET_KEY")
-app.config["UPLOAD_FOLDER"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static", "uploads")
-
-app.register_blueprint(user_bp)
-app.register_blueprint(event_bp)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "user.login"
 
 @login_manager.user_loader
 def load_user(user_id):
     return DataBase(User).firstWhere("id", user_id)
 
+
 # Handlers
 @app.errorhandler(404)
 def notFound(e):
     return render_template("404.html")
+
 
 # Routes
 @app.route("/")
@@ -33,7 +21,9 @@ def home():
     query = request.args.get("q")
 
     if query:
-        events = DataBase(Event, load_with=(User, "id", "user_id")).WhereLike("name", query)
+        events = DataBase(Event, load_with=(User, "id", "user_id")).WhereLike(
+            "name", query
+        )
     else:
         events = DataBase(Event, load_with=(User, "id", "user_id")).all()
 
---
+++ config.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_mail import Mail
+from routes import event_bp, user_bp
+from flask_mail import Mail, Message
+from flask_login import LoginManager, login_required, current_user
+from flask import render_template, request
+import os
+
+app = Flask(__name__)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "your_mail@gmail.com"
+app.config["MAIL_PASSWORD"] = "your_email_password"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = Truemail = Mail(app)
+
+
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "static", "uploads"
+)
+
+app.register_blueprint(user_bp)
+app.register_blueprint(event_bp)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "user.login"
